6e9d3a4f24a4f1357252d362f5f4f559968e34f0
listenyoon <listenyoon@gmail.com>
2023-02-24 00:44:03 +0900

    mod: 수입지출 첫번째 랜딩 제작

--- src/assets/emoji.js
+++ src/assets/emoji.js
@@ -1,10 +1,10 @@
-const emoji = {
+const emojiPrev = {
   급여: "💰",
   용돈: "👛",
   식비: "🌭",
   교통: "🚗",
   문화생활: "🎬",
-  생필품: "✏️",
+  생필품: "🧻",
   마트: "🛒",
   교육: "📚",
   통신: "📱",
@@ -17,5 +17,23 @@ const emoji = {
   카드: "💳",
   현금: "💵",
 };
+
+const emoji = [
+  { type: "급여", emoji: "💰" },
+  { type: "용돈", emoji: "👛" },
+  { type: "기타", emoji: "💬" },
+  { type: "식비", emoji: "🌭" },
+  { type: "교통", emoji: "🚗" },
+  { type: "문화생활", emoji: "🎬" },
+  { type: "생필품", emoji: "🧻" },
+  { type: "패션/미용", emoji: "👕" },
+  { type: "교육", emoji: "📚" },
+  { type: "의료/건강", emoji: "🏥" },
+  { type: "주거/통신", emoji: "🏠" },
+  { type: "경조사/회비", emoji: "💵" },
+  { type: "가전", emoji: "🖥" },
+  { type: "공과금", emoji: "🧾" },
+  { type: "기타", emoji: "💬" },
+];
 
 export { emoji };
--- src/pages/Floating/InputFlowPage.js
+++ src/pages/Floating/InputFlowPage.js
@@ -2,13 +2,33 @@ import React, { useState, useEffect } from "react";
 import PageStyle from "../../assets/pageStyle.module.css";
 import DateHeader from "../../components/DateHeader";
 import { ReactComponent as CalendarIcon } from "../../assets/calendarIcon.svg";
+import { ReactComponent as InputClearIcon } from "../../assets/inputClearIcon.svg";
 import styled from "styled-components";
 import { format } from "date-fns";
 import ko from "date-fns/locale/ko";
+import "./InputFlowPage.css";
+import SelectType from "../../components/FloatingPart/SelectType";
+import BottomButton from "../../components/buttons/BottomButton";
+import CustomSwitch from "../../components/buttons/CustomSwitch";
 
 function InputFlowPage() {
+  const [isIncome, setIsIncome] = useState(false);
   const [date, setDate] = useState(new Date());
-  const [price, setPrice] = useState("");
+  const [cost, setCost] = useState("");
+  const [used, setUsed] = useState("");
+  const [category, setCategory] = useState("");
+
+  const onSelectSwitch = (value) => {
+    if (value === 1) {
+      setIsIncome(true);
+      setUsed(1);
+    } else {
+      setIsIncome(false);
+      setUsed("");
+    }
+    setCost("");
+    setCategory("");
+  };
 
   return (
     <div className={PageStyle.floating_page_style}>
@@ -20,24 +40,36 @@ function InputFlowPage() {
         </StyledDiv>
       </DateHeader>
       <div className={PageStyle.content_style}>
-        <div>
-          <div>지출 금액</div>
-          <input
-            placeholder="0원"
-            type="text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value + "원")}
-          />
-        </div>
-        <div>
-          <div>사용처</div>
-          <input
+        <CustomSwitch selectionMode={2} onSelectSwitch={onSelectSwitch} />
+        <InputComponent
+          name={isIncome ? "수입 금액" : "지출 금액"}
+          placeholder="0원"
+          value={cost}
+          onChange={(value) => setCost(value)}
+        />
+        {!isIncome && (
+          <InputComponent
+            name="사용처"
             placeholder="어디에 쓰셨나요?"
-            type="text"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
+            value={used}
+            onChange={(value) => setUsed(value)}
+          />
+        )}
+
+        <div className="input-flow-input-box">
+          <div className="input-flow-name">카테고리</div>
+          <SelectType
+            type={isIncome}
+            prevData={category}
+            sendData={(value) => setCategory(value)}
           />
         </div>
+
+        <BottomButton
+          text="다음으로"
+          onClick={() => {}}
+          disabled={cost !== "" && category !== "" && used !== ""}
+        />
       </div>
     </div>
   );
@@ -56,23 +88,38 @@ const StyledDiv = styled.div`
   }
 `;
 
-function InputCpnt({ text, placeholder, value, onChange, keyboard }) {
-  const onChangeFunction = (e) => {
-    if (keyboard === "number") {
-      format({ phone: e.target.value.replace(/[^0-9]/g, "") });
-    }
-    onChange(e.target.value);
-  };
+const InputComponent = ({ name, placeholder, value, onChange }) => {
+  const [text, setText] = useState(value);
+  const [focus, setFocus] = useState(false);
+
+  useEffect(() => {
+    onChange(text);
+  }, [text]);
 
   return (
-    <div>
-      <div>{text}</div>
-      <input
-        placeholder={placeholder}
-        type="text"
-        value={value}
-        onChange={onChangeFunction}
-      />
+    <div className="input-flow-input-box">
+      <div className="input-flow-name">{name}</div>
+      <div className="input-flow-input-flex">
+        <input
+          className="input-flow-input"
+          placeholder={placeholder}
+          type={name === "사용처" ? "text" : "number"}
+          value={value}
+          onChange={(e) => setText(e.target.value)}
+          onBlur={() => {
+            setTimeout(() => {
+              setFocus(false);
+            }, 100);
+          }}
+          onFocus={() => setFocus(true)}
+        />
+        {focus && (
+          <InputClearIcon
+            className="input-flow-clear"
+            onClick={() => setText("")}
+          />
+        )}
+      </div>
     </div>
   );
-}
+};
